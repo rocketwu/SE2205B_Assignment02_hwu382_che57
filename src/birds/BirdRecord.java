c8460ef6961a09a1a2051008bb398155ad0d2a5d
hwu382@laptop <hwu382@uwo.ca>
2018-03-23 11:15:48 -0400

    finished BirdRecord.java

--- src/birds/BirdRecord.java
+++ src/birds/BirdRecord.java
@@ -5,16 +5,39 @@ package birds;
  * parts: a DataKey and the data associated with the DataKey.
  */
 public class BirdRecord {
-
- 
+    private DataKey key;
+    private String about;
+    private String sound;
+    private String image;
 
     // default constructor
     public BirdRecord() {
-       
-        
+
     }
 
      // Other constructors
+    public BirdRecord(DataKey key, String about, String sound, String image){
+        this.key=key;
+        this.about=about;
+        this.sound=sound;
+        this.image=image;
+    }
+    
+    public DataKey getDataKey(){
+        return this.key;
+    }
+    
+    public String getAbout(){
+        return this.about;
+    }
+    
+    public String getSound(){
+        return this.sound;
+    }
+    
+    public String getImage(){
+        return this.image;
+    }
  
 
 
